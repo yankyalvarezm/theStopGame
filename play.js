401ac08559a1d9f4aa84a568dd0cd37e7937db27
yankyalvarezm <143447867+yankyalvarezm@users.noreply.github.com>
2023-09-11 17:09:43 -0400

    Added categories and new features...
    Addes catergories, a function to remove the restart btn when timer hits 0 and a new logic to rest the gamestart coundown

--- play.js
+++ play.js
@@ -2,11 +2,9 @@ document.getElementById('stop').addEventListener('click', function () {
     window.location.href = './index.html'
 })
 
-document.getElementById('stop').addEventListener('click', function () {
-    window.location.href = './index.html'
-})
-
 console.log('play.js connected')
+
+
 
 // ? Functions 
 
@@ -22,20 +20,90 @@ function elClassCont(tag, className, content, parent) {
     return element;
 }
 
-let timeLeft = 10;
+let timeLeft = 5;
+let timeoutId = null;
 
 function countdown() {
-    timeLeft--;
+    const clickMe = document.getElementsByClassName('click-me')[0];
+
     document.getElementById("seconds").innerHTML = String(timeLeft);
+
     if (timeLeft > 0) {
-        setTimeout(countdown, 1000);
+        timeLeft--;
+        // console.log(clickMe)
+
+        clickMe.addEventListener('click', function () {
+
+            clearTimeout(timeoutId)
+            const seconds = document.getElementById('seconds')
+            seconds.textContent = '0';
+            console.log('stop')
+
+        })
+        // console.log("line 27")
+        timeoutId = setTimeout(countdown, 1000);
+    } else if (timeLeft === 0) {
+        console.log("line 30")
+
+        removeRestart()
+        resetTimer(30)
+        timeoutId = setTimeout(countdown, 1000);
+
+        Array.from(document.getElementsByClassName('click-me')).forEach(el => {
+            el.style.backgroundColor = 'white';
+            el.style.color = 'black';
+            el.innerText = "S T O P";
+
+            el.addEventListener('mouseenter', function () {
+                this.style.backgroundColor = 'red';
+                this.style.color = '#FFFFFF';
+                this.innerText = "S T O P";
+            });
+
+            el.addEventListener('mouseleave', function () {
+                this.style.backgroundColor = 'white';
+                this.style.color = 'black';
+                this.innerText = "S T O P";
+            });
+        });
+        console.log('line 35');
+
+        const gameplayDescription = elClass('h3', 'description-gameplay')
+        gameplayDescription.innerHTML = `Try to fill as much categories as you can, good luck!`;
+
+        // Gameplay Section
+
+        const roundTitle = elClassCont('DIV', 'small-rectangle round-title', `ROUND ${1}`, body);
+
+        const gameplayContainer = elClassCont('DIV', 'gameplay-container', '', body);
+
+
+        const headingsContainer = elClassCont('DIV', 'headings-container', '', gameplayContainer);
+
+        const catContainer = elClassCont('DIV', 'cat-container', '', headingsContainer);
+        const countryCat = elClassCont('DIV', 'small-rectangle categories', 'Country', catContainer);
+        const sportCat = elClassCont('DIV', 'small-rectangle categories', 'Sport', catContainer);
+        const professionCat = elClassCont('DIV', 'small-rectangle categories', 'Profession', catContainer);
+
+        const nameScoreContainer = elClassCont('DIV', 'nameScore-container', '', headingsContainer);
+        const playerName = elClassCont('DIV', 'small-rectangle nameScore', 'Player', nameScoreContainer);
+        const score = elClassCont('DIV', 'small-rectangle nameScore', 'Score', nameScoreContainer);
+
+
+
+
+
+
+
     }
+
 };
 
 // ? HTML Structure
 
 const title = document.querySelector('h1');
-const main = elClass('MAIN', 'container')
+const main = elClass('MAIN', 'container');
+const body = document.querySelector('body');
 
 const generators = elClassCont('SECTION', 'generators', '', main)
 elClassCont('DIV', 'small-rectangle', 'A', generators);
@@ -61,6 +129,7 @@ function startSequence(sideContainer) {
 
         if (loops === maxLoops) {
             clearInterval(intervalId);
+
             const randomIndex = Math.floor(Math.random() * lettersArray.length);
             document.querySelector('.small-rectangle').innerText = lettersArray[randomIndex];
             Array.from(document.getElementsByClassName('click-me')).forEach(el => {
@@ -69,6 +138,7 @@ function startSequence(sideContainer) {
             });
 
             const restartBtn = document.getElementsByClassName('restart')[0];
+
             if (!restartBtn) {
                 elClassCont('DIV', 'small-rectangle restart', 'Restart', sideContainer);
             }
@@ -85,28 +155,37 @@ function startSequence(sideContainer) {
                 const timerSpan = elClassCont('SPAN', '', '10', timer);
                 timerSpan.id = 'seconds';
                 countdown();
-
             }
 
             document.getElementsByClassName('restart')[0].addEventListener('click', function () {
                 console.log('restart')
-
+                timeLeft = 6;
                 startSequence(sideContainer);
-                timeLeft = 10;
             });
         }
     }, 50);
 }
 
 document.getElementsByClassName('click-me')[0].addEventListener('click', function () {
-    console.log('btn click - test');
-
-
-
     if (this.disabled) {
         return;
     }
 
     this.disabled = true;
     startSequence(sideContainer);
+
 });
+
+function removeRestart() {
+    let restartButton = document.getElementsByClassName('restart')[0]
+    let parent = document.getElementsByClassName('side-container')[0]
+
+    parent.removeChild(restartButton)
+}
+
+function resetTimer(seconds) {
+    timeLeft = seconds;
+}
+
+
+elClass('hr', 'hr-main');
